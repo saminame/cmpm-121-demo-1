5a1618fcddf7de48ae46d4348c2ceef6f52ef607
saminame <sesteqla@ucsc.edu>
2024-10-16 13:37:01 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,12 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Step 1: A button you can click
+const button = document.createElement("button");
+button.innerHTML = "🦢";
+button.style.fontSize = "30px";
+button.style.padding = "20px";
+
+app.append(button);
+
